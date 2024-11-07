9a97aa0eb76d74eeb21fbb63983b8031d277778b
Senan <senanaslan29@t-online.de>
2024-11-07 23:51:15 +0100

    Added PlaceLocation Model

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -2,10 +2,22 @@ import 'dart:io';
 
 import 'package:uuid/uuid.dart';
 
+class PlaceLocation {
+  PlaceLocation(
+      {required this.latitude, required this.longitude, required this.address});
+
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
 class Place {
-  Place(this.name, this.picture) : id = const Uuid().v4();
+  Place(
+      {required this.name, required this.picture, required this.placeLocation})
+      : id = const Uuid().v4();
 
   final String name;
   final String id;
   final File picture;
+  final PlaceLocation placeLocation;
 }
